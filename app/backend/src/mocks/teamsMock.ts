47bfff43f6f72df2843995249e95c1bdac87e094
Bárbara <babivaldez@gmail.com>
2023-09-27 19:07:48 -0300

    começa o desenvolvimento dos testes de backend

---
+++ app/backend/src/mocks/teamsMock.ts
@@ -0,0 +1,16 @@
+const listaDeTimesFindAll = [
+  {
+    id: 1,
+    teamName: 'Cruzeiro',
+  },
+  {
+    id: 2,
+    teamName: 'Bahia',
+  },
+  {
+    id: 3,
+    teamName: 'Botafogo',
+  },
+];
+
+export default listaDeTimesFindAll;
---
+++ app/backend/src/tests/teams.test.ts
@@ -0,0 +1,44 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import Example from '../database/models/ExampleModel';
+
+import { Response } from 'superagent';
+import SequelizeTeamModel from '../database/models/TeamModel';
+import listaDeTimesFindAll from '../mocks/teamsMock';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Verifica as rotas de /teams', () => {
+  beforeEach(function () { sinon.restore(); });
+  it('Verifica se uma lista de times é retornada', async function () {
+    sinon.stub(SequelizeTeamModel, 'findAll').resolves([
+      SequelizeTeamModel.build({ "id": 1, "teamName": "Cruzeiro"}),
+      SequelizeTeamModel.build({ "id": 2, "teamName": "Bahia"}),
+      SequelizeTeamModel.build({ "id": 3, "teamName": "Botafogo"}),
+    ]);
+
+    const response = await chai.request(app).get('/teams');
+
+    expect(response.status).to.be.equal(200);
+    expect(response.body).to.be.deep.equal(listaDeTimesFindAll);
+  });
+
+  it('Verifica se um time específico é retornado corretamente', async function () {
+    sinon.stub(SequelizeTeamModel, 'findAll').resolves([
+      SequelizeTeamModel.build({ "id": 1, "teamName": "Cruzeiro"}),
+      SequelizeTeamModel.build({ "id": 2, "teamName": "Bahia"}),
+      SequelizeTeamModel.build({ "id": 3, "teamName": "Botafogo"}),
+    ]);
+
+    const response = await chai.request(app).get('/teams');
+
+    expect(response.status).to.be.equal(200);
+    expect(response.body).to.be.deep.equal(listaDeTimesFindAll);
+  });
+});
