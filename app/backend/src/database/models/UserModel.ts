dfe4ce0b013246f91fcf13556a4fb181a7fec130
Bárbara <babivaldez@gmail.com>
2023-09-28 17:29:26 -0300

    cria a migration a a model para a tabela de usuários

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,49 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+class SequelizeUserModel extends Model<InferAttributes<SequelizeUserModel>,
+InferCreationAttributes<SequelizeUserModel>> {
+  declare id: CreationOptional<number>;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+SequelizeUserModel.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+  underscored: true,
+});
+
+export default SequelizeUserModel;
