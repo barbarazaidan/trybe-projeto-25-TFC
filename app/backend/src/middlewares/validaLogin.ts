f3b6f0a0c38f9a12a89306c5e767bc41db19b222
Bárbara <babivaldez@gmail.com>
2023-09-28 19:53:15 -0300

    finaliza as validações de email do login

--- app/backend/src/middlewares/validaLogin.ts
+++ app/backend/src/middlewares/validaLogin.ts
@@ -1,9 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
 
+const validaEmail = (email: string) => {
+  const emailValido = /\S+@+\w+\.+[c]+[o]+[m]/;
+  const isEmailValido = emailValido.test(email);
+  return isEmailValido;
+};
+
 async function validaLogin(req: Request, res: Response, next: NextFunction) {
   const { email, password } = req.body;
   if (!email || !password) {
     return res.status(400).json({ message: 'All fields must be filled' });
+  }
+
+  if (!validaEmail(email)) {
+    return res.status(401).json({ message: 'Invalid email or password' });
   }
   next();
 }
